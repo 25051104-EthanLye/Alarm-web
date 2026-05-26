640a3b667d29747edc568b18d2cd596ec92fce3f
Ethan Lye <25051104@myrp.edu.sg>
2026-05-26 12:41:06 +0800

    Added Express server and EJS views

--- app.js
+++ app.js
@@ -1,10 +1,42 @@
-const express = require('express');
-const app = express();
-const port = 3000;  
+// --- UPDATED & NEW ROUTES FOR APP.JS ---
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+// 2. Manage Alarms Page (Updated to handle 'Edit' mode)
+app.get('/manage', (req, res) => {
+    const message = req.query.msg || null;
+    const editId = req.query.edit || null;
+    
+    // If the user clicked "Edit", find that specific alarm to pre-fill the form
+    let editAlarm = null;
+    if (editId) {
+        editAlarm = alarms.find(a => a.id === editId);
+    }
+
+    res.render('manage', { alarms, message, editAlarm });
 });
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-}); 
+
+// 5. Edit an Existing Alarm (NEW)
+app.post('/edit-alarm/:id', (req, res) => {
+    const { title, description, date, time } = req.body;
+    const alarmIndex = alarms.findIndex(a => a.id === req.params.id);
+    
+    if (alarmIndex !== -1) {
+        // Update the existing alarm's data, but keep its original ID and status
+        alarms[alarmIndex] = {
+            ...alarms[alarmIndex],
+            title,
+            description,
+            date,
+            time
+        };
+        res.redirect('/manage?msg=Alarm successfully updated!');
+    } else {
+        res.redirect('/manage?msg=Error: Alarm not found.');
+    }
+});
+
+// 6. Delete an Alarm (NEW)
+app.post('/delete-alarm/:id', (req, res) => {
+    // Filter out the alarm with the matching ID
+    alarms = alarms.filter(a => a.id !== req.params.id);
+    res.redirect('/manage?msg=Alarm deleted.');
+});
